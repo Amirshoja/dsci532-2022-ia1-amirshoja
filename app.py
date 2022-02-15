7f9889b02df6e7753fac8b4952a3edc17626fe43
Amirshoja <ashojakhani@gmail.com>
2022-02-14 16:58:26 -0800

    Added app

---
+++ app.py
@@ -0,0 +1,41 @@
+from vega_datasets import data
+import altair as alt
+from dash import Dash, html, dcc, Input, Output
+
+# loading the cars data
+movies = data.movies()
+
+app = Dash(
+    __name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+)
+app.layout = html.Div(
+    [
+        html.Iframe(
+            id="scatter",
+            style={"border-width": "0", "width": "100%", "height": "400px"},
+        ),
+        dcc.Dropdown(
+            id="xcol-widget",
+            value="Production_Budget",
+            options=[{"label": col, "value": col} for col in movies.columns],
+        ),
+    ]
+)
+
+
+@app.callback(Output("scatter", "srcDoc"), Input("xcol-widget", "value"))
+def plot_movies(xcol):
+    chart = (
+        alt.Chart(movies)
+        .mark_point()
+        .encode(x=xcol, y="Worldwide_Gross", tooltip="Title")
+        .interactive()
+    )
+    return chart.to_html()
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
+
+
+data.list_datasets()
